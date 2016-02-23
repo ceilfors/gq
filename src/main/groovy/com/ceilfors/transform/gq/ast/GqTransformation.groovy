f27065b9df37eb62b9ca51e93b68388ba2daffbe
ceilfors <wisen@ceilfors.com>
2016-02-23 18:04:17 +0000

    Refactor to make method more readable by splitting the AST Spec chunks to multiple methods.

--- src/main/groovy/com/ceilfors/transform/gq/ast/GqTransformation.groovy
+++ src/main/groovy/com/ceilfors/transform/gq/ast/GqTransformation.groovy
@@ -1,13 +1,10 @@
 package com.ceilfors.transform.gq.ast
 
 import com.ceilfors.transform.gq.GqUtils
-import org.codehaus.groovy.ast.ASTNode
-import org.codehaus.groovy.ast.AnnotatedNode
-import org.codehaus.groovy.ast.MethodNode
-import org.codehaus.groovy.ast.Parameter
-import org.codehaus.groovy.ast.VariableScope
+import org.codehaus.groovy.ast.*
 import org.codehaus.groovy.ast.builder.AstBuilder
 import org.codehaus.groovy.ast.expr.ClosureExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
 import org.codehaus.groovy.ast.stmt.BlockStatement
 import org.codehaus.groovy.ast.stmt.Statement
 import org.codehaus.groovy.control.CompilePhase
@@ -15,17 +12,30 @@ import org.codehaus.groovy.control.SourceUnit
 import org.codehaus.groovy.transform.AbstractASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
 
+import static org.codehaus.groovy.ast.tools.GeneralUtils.returnS
+import static org.codehaus.groovy.ast.tools.GeneralUtils.varX
+
 @GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
 public class GqTransformation extends AbstractASTTransformation {
-
-    public static final String RESULT = "result"
 
     public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
         init(astNodes, sourceUnit)
 
         AnnotatedNode annotatedNode = astNodes[1] as AnnotatedNode
         if (annotatedNode instanceof MethodNode) {
-            transformMethodNode(annotatedNode)
+            MethodNode methodNode = annotatedNode as MethodNode
+
+            def result = varX("result")
+            def wrappedOriginalCode = wrapInClosure(methodNode)
+
+            BlockStatement newCode = new BlockStatement([
+                    printMethodHeaderS(methodNode),
+                    callClosureAndKeepResultS(wrappedOriginalCode, result),
+                    printVariableS(result),
+                    returnS(result)
+            ], new VariableScope())
+
+            methodNode.code = newCode
         } else {
             throw new IllegalStateException("Gq annotation is only usable in methods.")
         }
@@ -47,10 +57,8 @@ public class GqTransformation extends AbstractASTTransformation {
         return closure
     }
 
-    private void transformMethodNode(MethodNode methodNode) {
-        def astBuilder = new AstBuilder()
-
-        List<ASTNode> nodes = astBuilder.buildFromSpec {
+    private Statement printMethodHeaderS(MethodNode methodNode) {
+        new AstBuilder().buildFromSpec {
             block {
                 expression {
                     staticMethodCall(GqUtils, "printToFile") {
@@ -74,33 +82,41 @@ public class GqTransformation extends AbstractASTTransformation {
                         }
                     }
                 }
-                expression {
-                    declaration {
-                        variable "result"
-                        token "="
-                        methodCall {
-                            expression.add(wrapInClosure(methodNode))
-                            constant "call"
-                            argumentList {}
-                        }
-
-                    }
-                }
-                expression {
-                    staticMethodCall(GqUtils, "printToFile") {
-                        argumentList {
-                            binary {
-                                constant "-> "
-                                token "+"
-                                variable RESULT
-                            }
-
-                        }
-                    }
-                }
-                returnStatement { variable RESULT }
             }
-        }
-        methodNode.code = nodes[0] as Statement
+        }[0] as Statement
+    }
+
+    private Statement callClosureAndKeepResultS(ClosureExpression closure, VariableExpression resultVariable) {
+        new AstBuilder().buildFromSpec {
+            expression {
+                declaration {
+                    expression.add(resultVariable)
+                    token "="
+                    methodCall {
+                        expression.add(closure)
+                        constant "call"
+                        argumentList {}
+                    }
+
+                }
+            }
+        }[0] as Statement
+    }
+
+    private Statement printVariableS(VariableExpression variableExpression) {
+        new AstBuilder().buildFromSpec {
+            expression {
+                staticMethodCall(GqUtils, "printToFile") {
+                    argumentList {
+                        binary {
+                            constant "-> "
+                            token "+"
+                            expression.add(variableExpression)
+                        }
+
+                    }
+                }
+            }
+        }[0] as Statement
     }
 }
